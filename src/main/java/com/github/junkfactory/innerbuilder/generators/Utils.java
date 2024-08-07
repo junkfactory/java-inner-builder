b4f882104b4679ae220c67aa0d8595c6e6aa07d6
Junk F. Actory <2998269+junkfactory@users.noreply.github.com>
2024-08-07 00:24:52 -0700

    Enhancements (#8)
    * Handle immutables in detecting addTo method
    
    * Add putTo map (or any class with a put(x,y) method

--- src/main/java/com/github/junkfactory/innerbuilder/generators/Utils.java
+++ src/main/java/com/github/junkfactory/innerbuilder/generators/Utils.java
@@ -2,18 +2,19 @@ package com.github.junkfactory.innerbuilder.generators;
 
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementFactory;
+import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiModifier;
+import com.intellij.psi.PsiParameter;
 import com.intellij.psi.PsiParameterList;
-import com.intellij.psi.PsiStatement;
 import com.intellij.psi.PsiType;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.util.PsiUtil;
 import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
 
 public class Utils {
     @NonNls
@@ -84,8 +85,54 @@ public class Utils {
         }
     }
 
-    static PsiStatement createReturnThis(@NotNull PsiElementFactory psiElementFactory, @Nullable PsiElement context) {
-        return psiElementFactory.createStatementFromText("return this;", context);
+
+    @Nullable
+    public static PsiMethod findAddMethod(PsiField field) {
+        return findMethod(field, "add", 1);
     }
+
+    @Nullable
+    public static PsiMethod findPutMethod(PsiField field) {
+        return findMethod(field, "put", 2);
+    }
+
+    @Nullable
+    public static PsiMethod findMethod(PsiField field, String methodName, int parameterCount) {
+        if (isFieldInitializedWithImmutableInstance(field)) {
+            return null;
+        }
+        var fieldClass = PsiUtil.resolveClassInClassTypeOnly(field.getType());
+        var methods = Optional.ofNullable(fieldClass)
+                .map(PsiClass::getAllMethods)
+                .orElseGet(() -> new PsiMethod[0]);
+        for (var method : methods) {
+            if (method.getName().equals(methodName) &&
+                    method.getParameterList().getParametersCount() == parameterCount) {
+                return method;
+            }
+        }
+        return null;
+    }
+
+    public static PsiType resolveGenericParameterType(PsiType fieldType, PsiParameter param) {
+        //resolve the generic type of the collection via the parameter type of the add method
+        return PsiUtil.resolveGenericsClassInType(fieldType)
+                .getSubstitutor()
+                .substitute(param.getType());
+    }
+
+    public static boolean isFieldInitializedWithImmutableInstance(PsiField field) {
+        var initializer = field.getInitializer();
+        if (null == initializer) {
+            return false;
+        }
+        var initializerType = initializer.getType();
+        if (null == initializerType) {
+            return false;
+        }
+        var initializerClass = PsiUtil.resolveClassInClassTypeOnly(initializerType);
+        return null != initializerClass && initializerClass.hasModifierProperty(PsiModifier.ABSTRACT);
+    }
+
 
 }
