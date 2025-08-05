01a6b753ef207bc075f7e57f8a2aeab5257b4f6a
junkfactory <2998269+junkfactory@users.noreply.github.com>
2025-08-04 17:19:36 -0700

    Update to intellij 252.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 
 dependencies {
     intellijPlatform {
-        create("IC", "2025.1")
+        create("IC", "LATEST-EAP-SNAPSHOT", useInstaller = false)
         testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
         // Add necessary plugin dependencies for compilation here, example:
