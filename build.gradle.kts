b055781bd57de49c7ec55115dd08da168f061768
junkfactory <2998269+junkfactory@users.noreply.github.com>
2026-03-26 21:08:45 -0700

    Add support for 261.* versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ intellijPlatform {
     pluginConfiguration {
         ideaVersion {
             sinceBuild = "242"
-            untilBuild = "253.*"
+            untilBuild = "261.*"
         }
     }
 
